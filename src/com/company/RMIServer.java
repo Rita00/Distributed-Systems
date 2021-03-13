7843820e0513aec17217dac374339229b2a44cb8
Rita <rita.lapao00@gmail.com>
2021-03-13 15:27:01 -0100

    Connection between RMI servers with UDP

--- src/com/company/RMIServer.java
+++ src/com/company/RMIServer.java
@@ -1,5 +1,13 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
@@ -17,14 +25,66 @@ public class RMIServer extends UnicastRemoteObject implements RMI {
     }
 
     static public boolean isPrimaryFunctional() {
+        DatagramSocket aSocket = null;
+        boolean ok = true;
         try {
-            RMI h = (RMI) LocateRegistry.getRegistry(7000).lookup("test");
-            String message = h.saySomething();
-            System.out.println(message);
-        } catch (Exception e) {
-            return false;
+            aSocket = new DatagramSocket();
+            System.out.print("Mensagem a enviar = ");
+
+            InetAddress aHost = InetAddress.getByName("127.0.0.1");
+            int serverPort = 7001;
+            byte[] m = "Pinging".getBytes();
+            DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
+            aSocket.send(request);
+            aSocket.setSoTimeout(200);
+            byte[] buffer = new byte[1000];
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
+            System.out.println("Recebeu: " + new String(reply.getData(), 0, reply.getLength()));
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+            ok = false;
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+            ok = false;
+        } finally {
+            if (aSocket != null) aSocket.close();
         }
-        return true;
+        return ok;
+    }
+
+    static public void initializeRMI() {
+        System.out.println("Becoming Primary Server!");
+        try {
+            RMIServer obj = new RMIServer();
+            Registry r = LocateRegistry.createRegistry(7000);
+            r.rebind("test", obj);
+            r.rebind("admin", obj);
+            System.out.println("RMI Server ready!");
+        } catch (RemoteException re) {
+            System.out.println("Exception in Server.main: " + re);
+        }
+    }
+
+    static public void initializeUDP() {
+        String s;
+        try (DatagramSocket aSocket = new DatagramSocket(7001)) {
+            System.out.println("Socket Datagram à escuta no porto 7001");
+            while (true) {
+                byte[] buffer = new byte[1000];
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                s = new String(request.getData(), 0, request.getLength());
+                System.out.println("Recebeu um ping: " + s);
+                buffer = "I'm Alive".getBytes();
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length, request.getAddress(), request.getPort());
+                aSocket.send(reply);
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -39,16 +99,7 @@ public class RMIServer extends UnicastRemoteObject implements RMI {
                 e.printStackTrace();
             }
         }
-
-        System.out.println("Becoming Primary Server!");
-        try {
-            RMIServer obj = new RMIServer();
-            Registry r = LocateRegistry.createRegistry(7000);
-            r.rebind("test", obj);
-            r.rebind("admin", obj);
-            System.out.println("RMI Server ready!");
-        } catch (RemoteException re) {
-            System.out.println("Exception in Server.main: " + re);
-        }
+        initializeRMI();
+        initializeUDP();
     }
 }

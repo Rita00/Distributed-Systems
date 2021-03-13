1b2e35eb2c425dfa3e3d6bb9bd00dadc743a71a3
Rita <rita.lapao00@gmail.com>
2021-03-13 15:28:34 -0100

    Remove unnecessary imports

--- src/com/company/RMIServer.java
+++ src/com/company/RMIServer.java
@@ -1,13 +1,10 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.nio.charset.StandardCharsets;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;

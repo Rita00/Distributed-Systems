edab49c141e0dd06de7c2d225d8f50bf67ddbd30
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-03-14 17:31:50 +0000

    update

--- src/pt/uc/dei/student/AdminConsole.java
+++ src/pt/uc/dei/student/AdminConsole.java
@@ -116,7 +116,7 @@ public class AdminConsole {
         input.close();
     }
     
-    public void main(String[] args) {
+    public static void main(String[] args) {
         try {
         	RMI rmiServer = (RMI) LocateRegistry.getRegistry(7000).lookup("admin");
             String message = rmiServer.saySomething();

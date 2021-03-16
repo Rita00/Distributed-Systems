c47c6ea3fd1c79f1fc5877dba259960d10e0cf4f
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-03-16 22:01:47 +0000

    bd diagram

---
+++ db/tentativa_v0.sql
@@ -0,0 +1,48 @@
+CREATE TABLE election (
+	id		 BIGINT,
+	title	 VARCHAR(512) NOT NULL,
+	type		 VARCHAR(512) NOT NULL,
+	description	 VARCHAR(512),
+	begin_date	 DATE NOT NULL,
+	end_date	 DATE NOT NULL,
+	department_id INTEGER NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE candidacy (
+	id		 BIGINT,
+	type	 VARCHAR(512) NOT NULL,
+	election_id BIGINT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (election_id) REFERENCES election(id)
+);
+
+CREATE TABLE person (
+	name		 VARCHAR(32) NOT NULL,
+	cc_number	 INTEGER,
+	cc_validity	 DATE NOT NULL,
+	address	 VARCHAR(512) NOT NULL,
+	phone	 INTEGER NOT NULL,
+	job		 VARCHAR(512) NOT NULL,
+	password	 VARCHAR(512),
+	department_id INTEGER NOT NULL,
+	PRIMARY KEY(cc_number),
+	FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE department (
+	id	 INTEGER,
+	name VARCHAR(512),
+	PRIMARY KEY(id),
+);
+
+CREATE TABLE candidacy_person (
+	candidacy_id	 BIGINT NOT NULL,
+	person_cc_number INTEGER,
+	PRIMARY KEY(person_cc_number),
+	FOREIGN KEY (candidacy_id) REFERENCES candidacy(id),
+	FOREIGN KEY (person_cc_number) REFERENCES person(cc_number)
+);
+
+

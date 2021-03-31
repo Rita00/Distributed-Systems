fc184da31c74914b4f26cdf92cdc035044eb95b2
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-03-31 14:14:48 +0100

    encapsulação no InfoElectors

--- src/pt/uc/dei/student/others/InfoElectors.java
+++ src/pt/uc/dei/student/others/InfoElectors.java
@@ -2,14 +2,31 @@ package pt.uc.dei.student.others;
 
 import java.io.Serializable;
 
+/**
+ * Classe do Objeto Infomação
+ *
+ * @author Dylan Gonçãoves Perdigão
+ * @author Ana Rita Rodrigues
+ */
 public class InfoElectors implements Serializable {
-    public int count;
-    public String dep_name;
-    public String election_title;
+    private int count;
+    private String dep_name;
+    private String election_title;
 
     public InfoElectors(int count, String dep_name, String election_title) {
         this.count = count;
         this.dep_name = dep_name;
         this.election_title = election_title;
     }
+    public int getCount() {
+        return count;
+    }
+    public String getDep_name() {
+        return dep_name;
+    }
+    public String getElection_title() {
+        return election_title;
+    }
+
+
 }

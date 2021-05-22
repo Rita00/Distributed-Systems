93aff2f2d1fdcec647f6a903d4da4c4f6f8b431d
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-05-22 22:06:49 +0100

    assim localhost

--- src/webServer/action/LoginFacebookAction.java
+++ src/webServer/action/LoginFacebookAction.java
@@ -84,7 +84,7 @@ public class LoginFacebookAction extends ActionSupport implements SessionAware {
                 .provider(FacebookApi2.class)
                 .apiKey(apiKey)
                 .apiSecret(apiSecret)
-                .callback("http://127.0.0.1:8080/webserver/loginFacebookForRealThisTime") // Do not change this.
+                .callback("http://localhost:8080/webserver/loginFacebookForRealThisTime") // Do not change this.
                 .scope("public_profile")
                 .build();
         /*
